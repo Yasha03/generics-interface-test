d450d06c1eeb7231514f4f1861a1c5992886f216
Yasha Ivanov <y291203@mail.ru>
2022-02-21 23:05:32 +0300

    start work

---
+++ myNumber.java
@@ -0,0 +1,19 @@
+public class myNumber<T> implements myNumberInterface{
+
+    public T summ(T num1, T num2){
+         // ....
+     }
+
+    public T subtract(T num1, T num2){
+        // ....
+    }
+
+    public T division(T num1, T num2){
+        // ....
+    }
+
+    public T multiplication(T num1, T num2){
+        // ....
+    }
+
+}
---
+++ myNumberInterface.java
@@ -0,0 +1,9 @@
+public interface myNumberInterface <T>{
+    public T summ(T num1, T num2);
+
+    public T subtract(T num1, T num2);
+
+    public T division(T num1, T num2);
+
+    public T multiplication(T num1, T num2);
+}
